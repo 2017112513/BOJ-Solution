e8ea97b757f40f5a939fbf60b22831db05bf786c
JISEOP LEE <71008721+easyseop@users.noreply.github.com>
2022-08-20 17:25:22 +0900

    0820

--- solution/PG_문제집 2번.py
+++ solution/PG_문제집 2번.py
@@ -31,4 +31,4 @@ def solution(want,number,discount):
     return answer
 
 
-solution(["banana", "apple", "rice", "pork", "pot"]	,[3, 2, 2, 2, 1],["chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot", "banana", "apple", "banana"]	)
+# solution(["banana", "apple", "rice", "pork", "pot"]	,[3, 2, 2, 2, 1],["chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot", "banana", "apple", "banana"]	)
