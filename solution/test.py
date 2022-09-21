2d000f009c4aad5de9b4bf4eecbeb6b58a84fbdf
JISEOP LEE <71008721+easyseop@users.noreply.github.com>
2022-09-21 19:32:52 +0900

    0921

--- solution/test.py
+++ solution/test.py
@@ -1,80 +1,56 @@
-def search_feed(sx,sy,level):
+def solution(info,query):
+    answer = []
+
+    return answer
+
+
+
+infos = ["java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"]
+query = ["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"]
+
+
+from collections import defaultdict
+
+total_dic = defaultdict(list)
+# food_dic,age_dic,sort_dic = defaultdict(list),defaultdict(list),defaultdict(list)
+
+for info in infos:
+        info = info.split(" ")
+        for lang in [info[0], "-"]:
+            for job in [info[1], "-"]:
+                for career in [info[2], "-"]:
+                    for food in [info[3], "-"]:
+                        total_dic[lang + job + career + food].append(int(info[4]))
+
+
+answer = []
+
+for q in query:
+    q = q.split()
+    lang,job,career,food,score = q[0],q[2],q[4],q[6],q[7]
+    lis = total_dic[lang+job+career+food]
+    lis = sorted(lis)
+    s,h = 0,len(lis)
     
-    feed_list = []
-
-    visited = [[0]*N for _ in range(N)]
-
-    d_feed = deque()
-    d_feed.append([sx,sy,0])
-
-    while d_feed:
-        x,y,cnt = d_feed.popleft()
-        
-        for i in range(4):
-            a,b = x+dx[i],y+dy[i]
-            if 0<=a<N and 0<=b<N and not visited[a][b] and arr[a][b]<=level:
-                if 0<arr[a][b]<level:
-                    visited[a][b] = 1
-                    feed_list.append([a,b,cnt+1])
-                
-                
-                else: 
-                    visited[a][b] = 1
-                    d_feed.append([a,b,cnt+1])
-
-
-    feed_list = sorted(feed_list,key=lambda x: (-x[2],-x[0],-x[1]))
-
-    if feed_list:
-        x,y,dis= feed_list.pop()
-        return x,y,dis
-                    
-    
-    return 0,0,0
-
-
-from collections import deque
-import sys
-input = sys.stdin.readline
-
-N = int(input())
-arr = [list(map(int,input().split())) for _ in range(N)]
-
-dx = [1,-1,0,0]
-dy = [0,0,1,-1]
-
-for i in range(N):
-    for j in range(N):
-
-        if arr[i][j] == 9:
-            sx,sy = i,j
-            arr[i][j] = 0
-
-def bfs(sx,sy):
-    
-    feed = 0
-    level = 2
-    answer = 0
-
-    while 1:
-    
-        fx,fy,distance = search_feed(sx,sy,level)
-        # print('dff',distance,fx,fy)
-        if distance == 0:
-            break
-
+    mid = 0
+    while s<h:
+        mid = (s+h)//2      
+        if int(lis[mid]) >= int(score):
+            h = mid 
         else:
-            arr[fx][fy] = 0
-            sx,sy = fx,fy
-            feed += 1 
-            if feed == level :
-                level +=1 
-                feed = 0
+            s = mid+1
 
-            answer += distance
-    return answer 
+    cnt = len(lis)-s
 
-  
+    print(cnt)
 
-a = bfs(sx,sy)
-print(a)
+
+
+
+
+    
+   
+
+
+    
+
